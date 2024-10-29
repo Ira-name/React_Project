3bb0afb3b6be4c4714bf4a2608e52c3fc4d1e01a
Ira-name <iryna.krush@oa.edu.ua>
2024-10-29 22:06:29 +0200

    Basic routing commit

---
+++ project/src/routes/BasicRoutes.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/home/HomePage";
+import UserPage from "../pages/users/UserPage";
+import Layout from "../components/layout/Layout";
+import NotFoundPage from "../components/NotFoundPage";
+import CartPage from "../pages/cart/CartPage";
+import ProductPage from "../pages/products/ProductPage";
+const BasicRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/users" element={<UserPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default BasicRoutes;
