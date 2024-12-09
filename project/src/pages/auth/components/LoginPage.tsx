11ca788c69069dc343754d39f046c731ad2ccaa3
Ira-name <iryna.krush@oa.edu.ua>
2024-12-09 19:12:46 +0200

    commit

--- project/src/pages/auth/components/LoginPage.tsx
+++ project/src/pages/auth/components/LoginPage.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./LoginForm";
 
 const LoginPage= () => (
   <div className="d-flex justify-content-center align-items-center">
-    <img src="./shop2.jpg" alt="ЛОГОТИП" className="mt-5 mr-5" />
+    <img src="./shop2.jpg" alt="ЛОГо" className="mt-5 mr-5" />
     <LoginForm />
   </div>
 );
